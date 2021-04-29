22b29e8e2ffd598b91a806252a1c7f1d96e7bcc5
Levi <j.newedi@gmail.com>
2021-04-29 06:44:25 -0230

    Update 4-Fit_iSSA.R Separate habitat models,  plot habitat models

--- R_scripts/4-Fit_iSSA.R
+++ R_scripts/4-Fit_iSSA.R
@@ -15,7 +15,8 @@ library(amt)
 library(glmmTMB)
 
 # Load data
-model_dat <- readRDS('output/model_dat.rds')
+model_dat <- readRDS('output/model_dat.rds') %>%
+  na.omit()
 
 # Set max iterations to 10^15 to aid convergence
 glmmTMBControl(optCtrl=list(iter.max=1e15,eval.max=1e15))
@@ -24,18 +25,25 @@ glmmTMBControl(optCtrl=list(iter.max=1e15,eval.max=1e15))
 # Step length model
 sl_covs <- c('log_sl_:cort_ng_g_sc:period', 'log_sl_:period', 'log_sl_',
              '(1 | step_id_)', '(0 + cort_ng_g_sc | id)')
-# Habitat model
-hab_covs <- c('cort_ng_g_sc:cover:period', 'cort_ng_g_sc:crop:period', 
-              'cover:period', 'crop:period',
-              '(1 | step_id_)', '(0 + cort_ng_g_sc + cover + crop | id)')
+# Cover model
+cover_covs <- c('cort_ng_g_sc:cover:period', 'cort_ng_g_sc:cover', 
+              'cover:period', 'cover',
+              '(1 | step_id_)', '(0 + cort_ng_g_sc + cover | id)')
+# Crop model
+crop_covs <- c('cort_ng_g_sc:crop:period', 'cort_ng_g_sc:crop', 
+                'crop:period', 'crop',
+                '(1 | step_id_)', '(0 + cort_ng_g_sc + crop | id)')
+
+hab_covs <- c('crop', 'cover', 'crop:period', 'cover:period',
+              '(1 | step_id_)', '(0 + cover + crop | id)')
 
 # Run models
-for(i in c('hab', 'sl')) {
+for(i in c('crop', 'cover', 'sl', 'hab')) {
   model_covs <- get(paste(i, 'covs', sep = '_'))
   # Set up model without fitting
   model_form <- glmmTMB(reformulate(model_covs, response = 'case_'),
                       family=poisson(), 
-                      data = stps, doFit=FALSE)
+                      data = model_dat, doFit=FALSE)
   # Set variance of random intercept to 10^6
   model_form$parameters$theta[1] <- log(1e6)
   nvar_parm <- length(model_form$parameters$theta)
@@ -59,23 +67,30 @@ ggplot(stps_summ, aes(x = cort_ng_g, y = log(mean_sl), col = period)) +
   geom_point() +
   geom_smooth(method = 'lm')
 
-# Plot habitat model
-tidy_hab_model <- broom.mixed::tidy(hab_model) %>%
-  # Get confidence intervals
-  mutate(lower = estimate - std.error*1.96,
-         upper = estimate + std.error*1.96) %>%
-  # Filter out unneeded coefficients
-  filter(! term == '(Intercept)') %>%
-  filter(! effect == 'ran_pars')
-# Plot
-ggplot(tidy_hab_model, aes(x = term, y = estimate)) +
-  geom_hline(yintercept = 0, linetype = 'dashed') +
-  geom_point() +
-  geom_errorbar(aes(ymin = lower, ymax = upper)) + 
-  theme(axis.text.x = element_text(angle = 45))
+# Plot habitat models
+for(i in c('crop', 'cover', 'hab')) {
+  tidy_hab_model <- broom.mixed::tidy(get(paste(i, 'model', sep = '_'))) %>%
+    # Get confidence intervals
+    mutate(lower = estimate - std.error*1.96,
+           upper = estimate + std.error*1.96) %>%
+    # Filter out unneeded coefficients
+    filter(! term == '(Intercept)') %>%
+    filter(! effect == 'ran_pars')
+  # Plot
+  hab_plot <- ggplot(tidy_hab_model, aes(x = term, y = estimate)) +
+    geom_hline(yintercept = 0, linetype = 'dashed') +
+    geom_point() +
+    geom_errorbar(aes(ymin = lower, ymax = upper)) + 
+    theme(axis.text.x = element_text(angle = 45))
+  # Assign
+  assign(paste(i, 'plot', sep = '_'), hab_plot)
+}
+
 
 # Save model outputs
-saveRDS(hab_model, 'output/hab_model_results.rds')
-saveRDS(sl_model, 'output/sl_model_results.rds')
+for(i in c('crop', 'cover', 'sl', 'hab')) {
+  saveRDS(get(paste(i, 'model', sep = '_')), 
+          paste('output/', i, '_model_results.rds', sep = ''))
+}
 
 
