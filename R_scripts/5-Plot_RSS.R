503acf08c5b1f06c7cae7cf243d3fac10d170bf6
Levi <j.newedi@gmail.com>
2021-05-04 15:55:17 -0230

    Update 5-Plot_RSS.R Add confidence intervals to RSS

--- R_scripts/5-Plot_RSS.R
+++ R_scripts/5-Plot_RSS.R
@@ -27,7 +27,8 @@ mean_ta <- mean(model_dat$cos_ta_, na.rm = T)
 mean_sl <- mean(model_dat$log_sl_)
 
 # Data frame for predicted data from loc x1
-x1 <- data.frame(cort_ng_g_sc = seq(from = med_cort, to = max_cort, length.out = 100),
+x1 <- data.frame(cort_ng_g_sc = seq(from = med_cort, to = max_cort, 
+                                    length.out = 100),
                       hab = 1,
                       step_id_ = NA,
                       cos_ta_ = mean_ta,
@@ -52,15 +53,25 @@ for(i in c('cover', 'crop')) {
     x2 <- x2 %>% 
              mutate(period = paste(j, 'calv', sep = '_'))
     # Predict and calculate
-    logp_1 <- predict(rss_mod, newdata = x1, type = 'link', re.form = NA)
-    logp_2 <- predict(rss_mod, newdata = x2, type = 'link', re.form = NA)
-    logRSS <- logp_1 - logp_2
+    logp_1 <- predict(rss_mod, newdata = x1, type = 'link', re.form = NA, se.fit = T)
+    logp_2 <- predict(rss_mod, newdata = x2, type = 'link', re.form = NA, se.fit = T)
+    # Log RSS
+    logRSS <- as.numeric(logp_1$fit) - as.numeric(logp_2$fit)
+    # Confidence intervals from model SE
+    upperRSS <- (as.numeric(logp_1$fit) + as.numeric(logp_1$se.fit)) - 
+      (as.numeric(logp_2$fit) + as.numeric(logp_2$se.fit))
+    lowerRSS <- (as.numeric(logp_1$fit) - as.numeric(logp_1$se.fit)) - 
+      (as.numeric(logp_2$fit) - as.numeric(logp_2$se.fit))
     # Assign predicted selection to new df for binding
     assign(paste(i, j, sep = '_'), 
            data.frame(period = j, 
                       habitat = i, 
-                      cort = seq(from = 1.045, to = 6.325, length.out = 100), 
-                      selection = logRSS))
+                      cort = seq(from = median(model_dat$cort_ng_g, na.rm = T)/1000, 
+                                 to = max(model_dat$cort_ng_g, na.rm = T)/1000, 
+                                 length.out = 100), 
+                      selection = logRSS,
+                      upper = upperRSS,
+                      lower = lowerRSS))
   }
   # Bind predicted selection to larger data frame
   all_rss <- all_rss %>%
@@ -69,9 +80,14 @@ for(i in c('cover', 'crop')) {
 }
 
 # Plot
+# tiff('figures/rss_gc__habitat.tiff', width = 10, height = 8, units = 'in', res = 300)
 ggplot(all_rss, aes(x = cort, y = exp(selection), group = period, col = period)) +
   geom_hline(yintercept = exp(0), linetype = 'dashed') +
   scale_colour_manual(values = c('#e4bb3f', '#5ac18e')) +
+  scale_fill_manual(values = c('#e4bb3f', '#5ac18e')) +
+  geom_ribbon(alpha = 0.3, col = NA,
+              aes(ymin = exp(selection) - exp(lower), 
+                  ymax = exp(selection) + exp(upper), fill = period)) +
   geom_line(size = 1) +
   theme(panel.background = element_rect(colour = 'black', fill = 'white'),
         axis.text = element_text(size = 15), 
