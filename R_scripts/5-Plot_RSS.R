ef9a254a65bddd7d7ebf463ae940f6932392d73e
Levi <j.newedi@gmail.com>
2021-06-02 15:10:31 -0230

    Add loop to bootstrap RSS

--- R_scripts/5-Plot_RSS.R
+++ R_scripts/5-Plot_RSS.R
@@ -4,7 +4,9 @@
 ### Calculate relative selection strength     ###
 ### for crop and cover habitats with          ###
 ### change in cortisol levels, comparing the  ###
-### pre- and post-calving periods             ###
+### pre- and post-calving periods, using      ###
+### bootstrapped model coefficients to get    ###
+### median and 95% CI RSS                     ###
 ###                                           ###
 ### Then, plot and visualize results          ###
 ###                                           ###
@@ -39,48 +41,68 @@ x2 <- x1 %>%
   mutate(cort_ng_g_sc = med_cort)
 
 # Loop to calculate RSS
-all_rss <- data.frame()
+# Separate into cover and crop models
 for(i in c('cover', 'crop')) {
-  # Rename hab column as habitat name
-  colnames(x1)[2] <- paste(i)
-  colnames(x2)[2] <- paste(i)
-  # Get the appropriate model
-  rss_mod <- get(paste(i, 'model', sep = '_'))
-  # Calculate log RSS between loc x1 and x2
-  for(j in c('pre', 'post')) {
-    x1 <- x1 %>% 
-             mutate(period = paste(j, 'calv', sep = '_'))
-    x2 <- x2 %>% 
-             mutate(period = paste(j, 'calv', sep = '_'))
-    # Predict and calculate
-    logp_1 <- predict(rss_mod, newdata = x1, type = 'link', re.form = NA, se.fit = T)
-    logp_2 <- predict(rss_mod, newdata = x2, type = 'link', re.form = NA, se.fit = T)
-    # Log RSS
-    logRSS <- as.numeric(logp_1$fit) - as.numeric(logp_2$fit)
-    # Confidence intervals from model SE
-    upperRSS <- (as.numeric(logp_1$fit) + as.numeric(logp_1$se.fit)) - 
-      (as.numeric(logp_2$fit) + as.numeric(logp_2$se.fit))
-    lowerRSS <- (as.numeric(logp_1$fit) - as.numeric(logp_1$se.fit)) - 
-      (as.numeric(logp_2$fit) - as.numeric(logp_2$se.fit))
-    # Assign predicted selection to new df for binding
-    assign(paste(i, j, sep = '_'), 
-           data.frame(period = j, 
-                      habitat = i, 
-                      cort = seq(from = median(model_dat$cort_ng_g, na.rm = T)/1000, 
-                                 to = max(model_dat$cort_ng_g, na.rm = T)/1000, 
-                                 length.out = 100), 
-                      selection = logRSS,
-                      upper = upperRSS,
-                      lower = lowerRSS))
+  # Initiate df to collect results
+  mod_rss <- data.frame()
+  # Set initial iteration
+  iteration <- 0
+  # Loop through each model separately
+  for(mods in list.files('output/model_boots/', pattern = i)) {
+    # Increase iteration number
+    iteration <- iteration + 1
+    # Rename hab column as habitat name
+    colnames(x1)[2] <- paste(i)
+    colnames(x2)[2] <- paste(i)
+    # Get the appropriate model
+    rss_mod <- readRDS(paste('output/model_boots/', mods, sep = ''))
+    # Calculate log RSS between loc x1 and x2
+    for(j in c('pre', 'post')) {
+      x1 <- x1 %>% 
+        mutate(period = paste(j, 'calv', sep = '_'))
+      x2 <- x2 %>% 
+        mutate(period = paste(j, 'calv', sep = '_'))
+      # Predict and calculate
+      logp_1 <- predict(rss_mod, newdata = x1, type = 'link', re.form = NA, se.fit = F)
+      logp_2 <- predict(rss_mod, newdata = x2, type = 'link', re.form = NA, se.fit = F)
+      # Log RSS
+      logRSS <- logp_1 - logp_2
+      # Assign predicted selection to new df for binding
+      assign(paste(i, j, sep = '_'), 
+             data.frame(period = j, 
+                        habitat = i, 
+                        cort = seq(from = median(model_dat$cort_ng_g, na.rm = T)/1000, 
+                                   to = max(model_dat$cort_ng_g, na.rm = T)/1000, 
+                                   length.out = 100), 
+                        iteration,
+                        logRSS))
+  
+    }
+    # Bind predicted selection to larger data frame
+    mod_rss <- mod_rss %>% 
+      rbind(get(paste(i, 'post', sep = '_'))) %>% 
+      rbind(get(paste(i, 'pre', sep = '_')))
+    
   }
-  # Bind predicted selection to larger data frame
-  all_rss <- all_rss %>%
-    rbind(get(paste(i, 'post', sep = '_'))) %>%
-    rbind(get(paste(i, 'pre', sep = '_')))
+  
+  # Assign
+  assign(paste(i, 'mod_rss', sep = '_'), mod_rss)
 }
 
+# Bind together
+all_rss <- cover_mod_rss %>%
+  rbind(crop_mod_rss) %>%
+# Calculate median and CI of RSS
+  group_by(period, habitat, cort) %>%
+  summarize(selection = median(logRSS),
+            lower = quantile(logRSS, probs = 0.05),
+            upper = quantile(logRSS, probs = 0.95))
+
+# Save RSS data
+saveRDS(all_rss, 'output/rss_results.rds')
+
 # Plot
-# tiff('figures/rss_gc__habitat.tiff', width = 10, height = 8, units = 'in', res = 300)
+# tiff('figures/rss_gc_habitat.tiff', width = 10, height = 8, units = 'in', res = 300)
 ggplot(all_rss, aes(x = cort, y = exp(selection), group = period, col = period)) +
   geom_hline(yintercept = exp(0), linetype = 'dashed') +
   scale_colour_manual(values = c('#e4bb3f', '#5ac18e')) +
@@ -90,9 +112,12 @@ ggplot(all_rss, aes(x = cort, y = exp(selection), group = period, col = period))
                   ymax = exp(selection) + exp(upper), fill = period)) +
   geom_line(size = 1) +
   theme(panel.background = element_rect(colour = 'black', fill = 'white'),
+        strip.background = element_rect(fill = 'white'),
+        strip.text = element_text(size = 18),
+        panel.grid = element_blank(),
         axis.text = element_text(size = 15), 
         axis.title = element_text(size = 18)) +
-  facet_wrap(~ habitat) +
+  facet_wrap(~ habitat, scales = 'free') +
   xlab('Fecal cortisol (microgram/g)') +
   ylab('log RSS for habitat')
 
