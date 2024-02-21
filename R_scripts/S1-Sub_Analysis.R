468a4b795f4cea857ef1e7df1b6de94e6fdac281
ljnewediuk <j.newedi@gmail.com>
2024-02-20 18:26:29 -0600

    Add supp analysis
    Tests for contribution of error in assigning estimated calving dates to iSSA model

--- R_scripts/S1-Sub_Analysis.R
+++ R_scripts/S1-Sub_Analysis.R
@@ -17,6 +17,11 @@ dna_IDs <- readRDS('input/final_sample_IDs.rds') %>%
   # pull out only DNA-identified
   filter(identification_type == 'dna' & ! is.na(uid))
 
+# Get uIDs for samples at least 5 days outside estimated calving events
+out_calv_IDs <- readRDS('output/horm_calv_dat.rds') %>%
+  left_join(uid_IDs) %>%
+  filter(d_to_calv > 5 | d_to_calv < -5)
+
 # Load data for iSSA
 model_dat <- readRDS('derived_data/issa_model_dat.rds') %>%
   # na.omit() %>%
@@ -28,9 +33,13 @@ model_dat <- readRDS('derived_data/issa_model_dat.rds') %>%
   # Add year
   mutate(year = factor(year(t1_)))
 
-# exclude samples assigned by model
+# exclude samples assigned by model for DNA-only model
 model_dat_dna <- model_dat %>%
   filter(uid %in% dna_IDs$uid)
+
+# exclude samples within 5 d of calving
+model_dat_out_calv <- model_dat %>%
+  filter(uid %in% out_calv_IDs$uid)
 
 # Load hormone data with calving dates and filter out non-gen identified
 horm_dat <- readRDS('output/horm_calv_dat.rds') %>%
@@ -66,6 +75,17 @@ model_form_dna$parameters$theta[1] <- log(1e6)
 # Fit model using large fixed variance
 model_fit_dna <- glmmTMB:::fitTMB(model_form_dna)
 
+# Set out-of-calving-event-only model without fitting
+model_form_out_of_calv <- suppressWarnings(
+  glmmTMB(reformulate(model_covs, response = 'case_'),
+          family = poisson(), 
+          map = list(theta = factor(c(NA, 1:3))),
+          data = model_dat_out_calv, doFit = F))
+# Set variance of random intercept to 10^6
+model_form_out_of_calv$parameters$theta[1] <- log(1e6)
+# Fit model using large fixed variance
+model_fit_out_of_calv <- glmmTMB:::fitTMB(model_form_out_of_calv)
+
 # Set year model without fitting
 model_form_y <- suppressWarnings(
   glmmTMB(reformulate(model_covs_y, response = 'case_'),
@@ -76,6 +96,14 @@ model_form_y <- suppressWarnings(
 model_form_y$parameters$theta[1] <- log(1e6)
 # Fit model using large fixed variance
 model_fit_y <- glmmTMB:::fitTMB(model_form_y)
+
+# Fit model to test if glucocorticoids higher or lower after calving
+mod <- brm(cort_ng_g_sc ~ period + factor(year) + (1 | animal_ID),
+           data = horm_dat, family = gaussian, 
+           iter = 10000, warmup = 5000, chains = 4, cores = 4, 
+           prior = prior(normal(0,1), class = b),
+           control = list(adapt_delta = 0.99, max_treedepth = 18),
+           backend = 'cmdstanr')
 
 # Fit model to test if glucocorticoids higher or lower after calving
 mod <- brm(cort_ng_g_sc ~ period + factor(year) + (1 | animal_ID),
